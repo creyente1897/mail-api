1102cff753a59e27780c3832a85f27dcd73ddbd2
creyente1897 <ayushsrivastava1897@gmail.com>
2019-01-09 23:12:37 +0530

    Added GET /mails/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
@@ -29,8 +30,27 @@ app.get('/mails', (req,res) => {
     res.send({mails});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
+
+app.get('/mails/:id', (req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Mail.findById(id).then((mail) =>{
+    if(!mail){
+      return res.status(404).send();
+    }
+
+    res.send({mail});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
