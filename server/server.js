69f8127489fa31d17cee91ede7ea893e186061b3
creyente1897 <ayushsrivastava1897@gmail.com>
2019-01-13 04:42:38 +0530

    Added POST /user/login route

--- server/server.js
+++ server/server.js
@@ -107,6 +107,19 @@ app.post('/users', (req,res) => {
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
 });
+
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+User.findByCredentials(body.email, body.password).then((user) => {
+  return user.generateAuthToken().then((token) => {
+    res.header('x-auth', token).send(user);
+  });
+}).catch((e) => {
+  res.status(400).send();
+});
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
